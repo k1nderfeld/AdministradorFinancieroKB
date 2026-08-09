6b96e0026eedc04548eafddd7e4ea273d0de59ce
poderpasado <baltazar.rodriguezt@gmail.com>
2026-08-09 01:25:36 -0400

    feat: Se definio el DTO UpdateUserDto para recibir datos actualizados del usuario

---
+++ BackEnd/src/DataTransferObjects/User/UpdateUserDto.ts
@@ -0,0 +1,8 @@
+export default interface UpdateUserDto {
+    name? : string,
+    lastName? : string,
+    phoneNumber? : string,
+    email? : string,
+    password? : string,
+    profilePicture? : string
+}
