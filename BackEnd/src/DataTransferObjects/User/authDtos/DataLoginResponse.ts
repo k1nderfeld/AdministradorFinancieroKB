664fcf2b4e48e03660195d0bbf2af22351007eb3
poderpasado <baltazar.rodriguezt@gmail.com>
2026-08-10 21:35:30 -0400

    feat: Implementación de logica en DTOs

--- BackEnd/src/DataTransferObjects/User/authDtos/DataLoginResponse.ts
+++ BackEnd/src/DataTransferObjects/User/authDtos/DataLoginResponse.ts
@@ -1,3 +1,8 @@
 export default interface DataLoginResponse {
-    
+    token : string,
+    id : number,
+    rut : string,
+    name : string,
+    lastName : string,
+    email : string
 }
--- BackEnd/src/DataTransferObjects/User/userDtos/RegisterUserResponse.ts
+++ BackEnd/src/DataTransferObjects/User/userDtos/RegisterUserResponse.ts
@@ -1,3 +1,4 @@
 export default interface RegisterUserResponse {
-    
+    response : boolean,
+    message : string
 }
