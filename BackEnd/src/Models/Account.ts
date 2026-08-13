0ae016b0ca9bfae2b6823740536548819ce146af
Baltazar Rodríguez <baltazar.rodriguezt@gmail.com>
2026-08-13 15:17:29 -0400

    feat: Implementacion de logica
    Fix constructor parameter destructuring and type checking.

--- BackEnd/src/Models/Account.ts
+++ BackEnd/src/Models/Account.ts
@@ -1,12 +1,22 @@
 import Card from "../src/Models/Card.js";
+import AccountProps from "../src/Models/Props/AccountProps.js";
 
 export default class Account {
-  id? : number;
-  name : string;
-  type : string;
-  wallet : Card[];
+    id? : number;
+    name : string;
+    type : string;
+    wallet : Card[];
   
   constructor ({
-    
-  })
+    id,
+    name,
+    type,
+    wallet
+  } : AccountProps) {
+    if (id !=== undefined) {
+      this.id = id;
+    }
+    this.name = name;
+    this.type = type;
+    this.wallet = wallet;
 }
