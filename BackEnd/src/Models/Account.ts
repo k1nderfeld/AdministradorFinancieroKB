99e23c67a1c9c60fd5bd908fee91ed764eb2e4af
Baltazar Rodríguez <baltazar.rodriguezt@gmail.com>
2026-08-13 14:36:00 -0400

    Add wallet property to Account model

---
+++ BackEnd/src/Models/Account.ts
@@ -0,0 +1,12 @@
+import Card from "../src/Models/Card.js";
+
+export default class Account {
+  id? : number;
+  name : string;
+  type : string;
+  wallet : Card[];
+  
+  constructor ({
+    
+  })
+}
