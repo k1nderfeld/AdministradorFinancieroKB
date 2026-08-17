bcfc55d534d1b5941819f3bd8a042773683ea2f9
poderpasado <baltazar.rodriguezt@gmail.com>
2026-08-17 11:25:40 -0400

    feat: Se implemento logica en AuthController

--- BackEnd/src/Controllers/AuthController.ts
+++ BackEnd/src/Controllers/AuthController.ts
@@ -1,3 +1,5 @@
+import type { Request, Response } from "express"; 
+import type RegisterUserDto from "../DataTransferObjects/User/userDtos/RegisterUserDto.js";
 import IAuthServices from "../Services/Interfaces/IAuthServices.js";
 
 export default class AuthController {
@@ -7,5 +9,12 @@ export default class AuthController {
         this._authServices = authServices;
     }
 
-    
+    async Register(req: Request, res: Response){
+        // Se saca el DTO del usuario a registrar
+        const registerUserDto: RegisterUserDto = req.body;
+        
+        const response = await this._authServices.Register(registerUserDto);
+        // Se establece el codigo HTTP en conjunto con el body(.json) como response
+        res.status(response.code).json(response);
+    }
 }
