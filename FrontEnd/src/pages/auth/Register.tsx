6e3d89aa683aa1a5a63843e530213ab655ae45ae
Baltazar Rodríguez Tapia <baltazar.rodriguezt@gmail.com>
2026-08-20 12:16:02 -0400

    Initialize registration form state
    Added state management for registration form.

--- FrontEnd/src/pages/auth/Register.tsx
+++ FrontEnd/src/pages/auth/Register.tsx
@@ -1,5 +1,18 @@
 import handleRegister from "../../services/User/registerService.js";
+import RegisterUserRequest from "../interfaces/register.js";
+import Input from "../components/ui/input";
 
 export default function Register() {
+    const [form, setForm] = useState<RequestUserResponse>({
+        rut: "",
+        name: "",
+        lastName: "",
+        birthDate: "",
+        phoneNumber: "",
+        email: ""
+    });
+    // método de navegación entre páginas
+    const navigate = useNavigate();
     
+    const [loading, setLoading] = useState(false);       
 }
